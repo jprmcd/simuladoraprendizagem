01a5b89d091c458ef4a11f132d5a8d5d76492506
João Paulo <jprmcd@gmail.com>
2017-06-06 10:19:44 -0300

    Create ciclo_simulador.class.php

---
+++ ciclo_simulador.class.php
@@ -0,0 +1,22 @@
+<?php
+public class ciclo_simulador{
+  private $id_sml;
+  private $descricao_csm;
+  private $imagem_csm;
+  
+  public function __set($atrib, $value){
+    $this->$atrib=$value;
+  }
+  public function __get($atrib){
+    return$this->$atrib;
+  }
+  public function __construct(){
+    $this->descricao_csm="";
+    $this->imagem_csm="";
+  }
+  public function __construct($nome,$descricao){
+    $this->descricao_csm=$descricao_csm;
+    $this_>imagem_csm=$imagem_csm;
+  }
+  }
+?>
